7f8da491d20975790edef073a4ba2cf1e5c4e02c
JHatch94 <35378910+JHatch94@users.noreply.github.com>
2019-11-02 16:09:14 -0600

    Commit before submission

---
+++ MyScriptureJournal/Models/Canon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class Canon
+    {
+        public int CanonId { get; set; }
+        public String CanonName { get; set; }
+
+
+    }
+}
+//context.[entity].select.tolist
--- MyScriptureJournal/Pages/Entries/Index.cshtml.cs
+++ MyScriptureJournal/Pages/Entries/Index.cshtml.cs
@@ -20,21 +20,43 @@ namespace MyScriptureJournal.Pages.Entries
         }
 
         public IList<Entry> Entry { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Canons { get; set; }
+        [BindProperty(SupportsGet = true)]
+        //public SelectList Books { get; set; }
         //[BindProperty(SupportsGet = true)]
-        //public string SearchString { get; set; }
-        //public SelectList Canons { get; set; }
-        //[BindProperty(SupportsGet = true)]
-        //public string ScriptureCanon { get; set; }
-
+        public string ScriptureCanon { get; set; }
+        //public string ScriptureBook { get; set; }
         public async Task OnGetAsync()
         {
-        //    var entries = from e in _context.Entry
-        //                 select e;
-        //    if (!string.IsNullOrEmpty(SearchString))
-        //    {
-        //        Entry = Entry.Where(s => s.Book.Contains(SearchString));
-        //    }
-        //
+            // Use LINQ to get list of genres.
+            IQueryable<string> canonQuery = from e in _context.Entry
+                                            orderby e.Canon
+                                            select e.Canon;
+          /*  IQueryable<string> bookQuery = from e in _context.Entry
+                                            orderby e.Book
+                                            select e.Book;*/
+
+            var entries = from e in _context.Entry
+                          select e;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                entries = entries.Where(s => s.Book.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(ScriptureCanon))
+            {
+                entries = entries.Where(x => x.Canon == ScriptureCanon);
+            }
+            Canons = new SelectList(await canonQuery.Distinct().ToListAsync());
+
+/*            if (!string.IsNullOrEmpty(ScriptureBook))
+            {
+                entries = entries.Where(x => x.Book == ScriptureBook);
+            }
+            Books = new SelectList(await bookQuery.Distinct().ToListAsync());*/
+
             Entry = await _context.Entry.ToListAsync();
         }
     }
